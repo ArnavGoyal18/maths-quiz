100c74a825ce67077c7191b2e765153580659176
ArnavGoyal18 <78960715+ArnavGoyal18@users.noreply.github.com>
2021-11-06 13:16:25 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    var player1=document.getElementById("player1_name_input");
-    var player2=document.getElementById("player2_name_input");
-    localStorage.setItem("player1_name",player1 );
-    localStorage.setItem("player2_name",player2 );
-window.location="quiz_game_page.html";
-}
