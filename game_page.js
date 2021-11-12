0d095bf4789859d163022a672a372c20ca66439f
ArnavGoyal18 <78960715+ArnavGoyal18@users.noreply.github.com>
2021-11-12 15:18:26 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,15 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    answer= parseInt(number1)* parseInt(number2);
+
+question_number="<h4>" + number1 + " X " + number2 +"</h4>";
+input_box="<br>Answer : <input type='text' id='input_check_box'>"
+check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row= question_number + input_box + check_button ;
+document.getElementById("output").innerHTML=row;
+document.getElementById("number1").value="";
+document.getElementById("number2").value="";
+}
